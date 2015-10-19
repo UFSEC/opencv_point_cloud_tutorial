91153a6bbfb9a43d6e1eb65b68c3b63c4bdbda64
Nick Tardif <nicktardif@gmail.com>
2015-10-19 18:11:18 +0000

    WIP step 4

--- 4_Single_Point_Cloud/vis.cc
+++ 4_Single_Point_Cloud/vis.cc
@@ -1,6 +1,11 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+
 #include <opencv2/viz.hpp>
 #include <opencv2/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <string>
 
 using namespace cv;
 using namespace std;
@@ -16,6 +21,35 @@ int main()
     /// Add coordinate axes
     myWindow.showWidget("Coordinate Widget", viz::WCoordinateSystem());
 
+    string image1_path = "../../../images/color.png";
+    string image2_path = "../../../images/depth.png";
+
+    string window1_title = "color";
+    string window2_title = "depth";
+
+    namedWindow(window1_title, WINDOW_AUTOSIZE);
+    namedWindow(window2_title, WINDOW_AUTOSIZE);
+
+    // Read in the images
+    Mat image1 = imread(image1_path);
+    Mat image2 = imread(image2_path);
+
+    imshow(window1_title, image1);
+    imshow(window2_title, image2);
+
+    waitKey(0);
+    return 0;
+    /*
+    // Create the camera calibration matrix
+    camera_matrix = np.zeros((3,3))
+    fx = 532.189488 
+    fy = 532.189488 
+    cx = 318.839986 
+    cy = 244.149197
+    scale = 10000.0
+    */
+
+    /*
     /// Add line to represent (1,1,1) axis
     viz::WLine axis(Point3f(-1.0f,-1.0f,-1.0f), Point3f(1.0f,1.0f,1.0f));
     axis.setRenderingProperty(viz::LINE_WIDTH, 4.0);
@@ -33,7 +67,7 @@ int main()
     float translation_phase = 0.0, translation = 0.0;
     while(!myWindow.wasStopped())
     {
-        /* Rotation using rodrigues */
+        // Rotation using rodrigues
         /// Rotate around (1,1,1)
         rot_vec.at<float>(0,0) += CV_PI * 0.01f;
         rot_vec.at<float>(0,1) += CV_PI * 0.01f;
@@ -53,6 +87,5 @@ int main()
 
         myWindow.spinOnce(1, true);
     }
-
-    return 0;
+    */
 }
