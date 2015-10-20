bf241642d3f02c1b48a9449b888e350ee0ece2df
Nick Tardif <nicktardif@gmail.com>
2015-10-20 03:21:32 +0000

    WIP still need transformation correct

--- 5_Multiple_Point_Cloud/multiple_point_cloud.cc
+++ 5_Multiple_Point_Cloud/multiple_point_cloud.cc
@@ -25,87 +25,118 @@ Matx33d getCameraMatrix() {
                   0.0, 0.0, 1.0);
 }
 
+// Filter the matches by an arbitrary distance
+vector<DMatch> filter_matches(vector<DMatch> matches, int max_distance) {
+  vector<DMatch> filtered_matches;
+  for(auto match : matches) {
+    //cout << "Distance: " << match.distance << endl;
+    if(match.distance < max_distance) filtered_matches.push_back(match);
+  }
+  return filtered_matches;
+}
+
+
 int main() {
   // Create a viz window
-  viz::Viz3d viz("Coordinate Frame");
+  viz::Viz3d viz("Fun Visualization :)");
 
-  string color_path = "../../../images/color.png";
-  string depth_path = "../../../images/depth.png";
-  string color2_path = "../../../images/color_2.png";
-  string depth2_path = "../../../images/depth_2.png";
-
-  // Read in the images
-  Mat color = imread(color_path, CV_LOAD_IMAGE_COLOR);
-  Mat depth = imread(depth_path, CV_LOAD_IMAGE_ANYDEPTH);
+  string color_1_path = "../../../images/color.png";
+  string depth_1_path = "../../../images/depth.png";
+  string color_2_path = "../../../images/color_2.png";
+  string depth_2_path = "../../../images/depth_2.png";
 
   Matx33d camera_matrix = getCameraMatrix();
 
-  Mat depth_cloud;
-  rgbd::depthTo3d(depth, camera_matrix, depth_cloud);
+  // Read in the images
+  Mat img_color_1 = imread(color_1_path, CV_LOAD_IMAGE_COLOR);
+  Mat img_depth_1 = imread(depth_1_path, CV_LOAD_IMAGE_ANYDEPTH);
+  Mat img_color_2 = imread(color_2_path, CV_LOAD_IMAGE_COLOR);
+  Mat img_depth_2 = imread(depth_2_path, CV_LOAD_IMAGE_ANYDEPTH);
 
+  // TODO: Get the feature points
+  Ptr<ORB> orb = ORB::create();
+  vector<KeyPoint> keypoints_1, keypoints_2;
+  Mat descriptors_1, descriptors_2;
 
-  Mat color2 = imread(color2_path, CV_LOAD_IMAGE_COLOR);
-  Mat depth2 = imread(depth2_path, CV_LOAD_IMAGE_ANYDEPTH);
+  orb->detect(img_color_1, keypoints_1);
+  orb->detect(img_color_2, keypoints_2);
+  orb->compute(img_color_1, keypoints_1, descriptors_1);
+  orb->compute(img_color_2, keypoints_2, descriptors_2);
 
-  imshow("color2", color2);
-  imshow("depth2", depth2);
+  Mat img_keypoints1, img_keypoints2;
+  drawKeypoints(img_color_1, keypoints_1, img_keypoints1, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
+  drawKeypoints(img_color_2, keypoints_2, img_keypoints2, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
 
-  Mat depth2_cloud;
-  rgbd::depthTo3d(depth2, camera_matrix, depth2_cloud);
+  // Match up the Feature Points between images
+  BFMatcher matcher(NORM_L2);
+  std::vector<DMatch> matches;
+  matcher.match(descriptors_1, descriptors_2, matches);
 
-  cout << "depth cloud rows: " << depth_cloud.rows << ", cols: " << depth_cloud.cols << endl;
-  cout << "depth cloud 2 rows: " << depth2_cloud.rows << ", cols: " << depth2_cloud.cols << endl;
+  const int max_match_distance = 200;
+  vector<DMatch> filtered_matches = filter_matches(matches, max_match_distance);
+  
+  Mat img_matches;
+  drawMatches(img_color_1, keypoints_1, img_color_2, keypoints_2, filtered_matches, img_matches);
+  //imshow("matches", img_matches);
+  //waitKey(0);
 
-  vector<Point3d> first, second;
-
-  for(int i = 0; i < depth_cloud.rows; i++) {
-    for(int j = 0; j < depth_cloud.cols; j++) {
-      Vec3d vec = depth_cloud.at<Vec3d>(i, j);
-      Point3d point(vec);
-      first.push_back(point);
-    }
+  vector<Point> query_matched_keypoints_vec, train_matched_keypoints_vec;
+  for(auto match: filtered_matches) {
+    Point query_keypoint = keypoints_1[match.queryIdx].pt;
+    Point train_keypoint = keypoints_2[match.trainIdx].pt;
+    query_matched_keypoints_vec.push_back(query_keypoint);
+    train_matched_keypoints_vec.push_back(train_keypoint);
+    //cout << "Query Idx: " << match.queryIdx << ", Train Idx: " << match.trainIdx << endl;
+    //cout << "Query Pt: " << query_keypoint << ", Train Pt: " << train_keypoint << endl;
   }
-  for(int i = 0; i < depth2_cloud.rows; i++) {
-    for(int j = 0; j < depth2_cloud.cols; j++) {
-      Vec3d vec = depth2_cloud.at<Vec3d>(i, j);
-      Point3d point(vec);
-      second.push_back(point);
-    }
-  }
+  Mat query_matched_keypoints(query_matched_keypoints_vec);
+  Mat train_matched_keypoints(train_matched_keypoints_vec);
 
+  // Select the matching keypoints and get their 3D representations
+  Mat query_keypoints_3d, train_keypoints_3d;
+  rgbd::depthTo3dSparse(img_depth_1, camera_matrix, query_matched_keypoints, query_keypoints_3d);
+  rgbd::depthTo3dSparse(img_depth_2, camera_matrix, train_matched_keypoints, train_keypoints_3d);
+
+  // TODO: Estimate the affine 3d transform between images using matching keypoints
   std::vector<uchar> inliers;
-  cv::Mat aff(4,4,CV_64F);
+  cv::Mat aff(3,4,CV_64F);
+  int ret = cv::estimateAffine3D(query_keypoints_3d, train_keypoints_3d, aff, inliers);
+  std::cout << "Transformation: " << endl << aff << std::endl;
 
-  int ret = cv::estimateAffine3D(first, second, aff, inliers);
+  // This is necesssary to calculate the inverse of the affine transformation
+/*
   Mat homogenous_row = Mat(1, 4, CV_64F);
   homogenous_row.at<double>(0, 0) = 0;
   homogenous_row.at<double>(0, 1) = 0;
   homogenous_row.at<double>(0, 2) = 0;
   homogenous_row.at<double>(0, 3) = 1;
   aff.push_back(homogenous_row);
+*/
 
-  std::cout << "Transformation: " << aff << std::endl;
+  Mat depth_1_cloud, depth_2_cloud;
+  rgbd::depthTo3d(img_depth_1, camera_matrix, depth_1_cloud);
+  rgbd::depthTo3d(img_depth_2, camera_matrix, depth_2_cloud);
 
-  Mat depth2_transformed(480, 640, CV_64FC3);
+  const int rows = 480;
+  const int cols = 640;
+  Mat depth_2_transformed(rows, cols, CV_64FC3);
 
-  for(int i = 0; i < depth2_cloud.rows; i++) {
-    for(int j = 0; j < depth2_cloud.cols; j++) {
-      Vec3d my_3d = depth2_cloud.at<Vec3d>(i, j);
-      Mat mat41 = Mat(4, 1, CV_64F);
+  for(int i = 0; i < depth_2_cloud.rows; i++) {
+    for(int j = 0; j < depth_2_cloud.cols; j++) {
+      Vec3d my_3d = depth_2_cloud.at<Vec3d>(i, j);
+      Mat mat41 = Mat::ones(4, 1, CV_64F);
       mat41.at<double>(0,0) = my_3d[0];
-      mat41.at<double>(1,0) = my_3d[1];
-      mat41.at<double>(2,0) = my_3d[2];
-      mat41.at<double>(3,0) = 1.0;
-
-      Mat result41 = aff.inv() * mat41;
-      Vec3d result_vec(result41.at<double>(0,0), result41.at<double>(1,0), result41.at<double>(2,0));
-      depth2_transformed.at<Vec3d>(i, j) = result_vec;
+      mat41.at<double>(0,1) = my_3d[1];
+      mat41.at<double>(0,2) = my_3d[2];
+      Mat result41 = aff * mat41;
+      Vec3d result_vec(result41.at<double>(0,0), result41.at<double>(0,1), result41.at<double>(0,2));
+      depth_2_transformed.at<Vec3d>(i, j) = result_vec;
     }
   }
   
   Mat depth_combined, color_combined;
-  hconcat(depth_cloud, depth2_transformed, depth_combined);
-  hconcat(color, color2, color_combined);
+  hconcat(depth_1_cloud, depth_2_transformed, depth_combined);
+  hconcat(img_color_1, img_color_2, color_combined);
 
   viz.showWidget("cloud", viz::WCloud(depth_combined, color_combined));
   viz.spin();
